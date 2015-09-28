b321ab9f3a420c44e7edc414e34b50d45c736c0f
bigredvella <matthew.j.vella@gmail.com>
2015-09-27 20:54:38 -0500

    Update install-webserver.sh

--- Code/install-webserver.sh
+++ Code/install-webserver.sh
@@ -3,6 +3,11 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
+git clone https://github.com/bigredvella/ITMO544.git
+
+mv ./bigredvella/ITMO544.git/images /var/www/html/images
+mv ./bigredvella/ITMO544.git/code /var/www/html
+
 
 
 echo “This worked! Go you, you coding rockstar!” > /tmp/Confirmation.txt
